88b648420b3748cd1271e655ac600eeab3e81b64
[abhijeet anand] <[anandabhijeet098@gmail.com]>
2024-01-21 18:52:16 +0530

    Implemented Number of Repos Per Page

--- public/script.js
+++ public/script.js
@@ -1,6 +1,15 @@
 // const {userData} from './constant.js'
 let currentPage = 1;
-const itemsPerPage = 10;
+let itemsPerPage = 10;
+
+function displaySelectedRepo() {
+  // Get the selected value from the dropdown
+  var selectedRepo = document.getElementById("repoSelector").value;
+
+  // Display a message with the selected repository number
+  itemsPerPage = selectedRepo;
+  // alert("Selected Repository: " + selectedRepo);
+}
 
 async function performSearch(event) {
   event.preventDefault();
@@ -9,6 +18,10 @@ async function performSearch(event) {
 
   const container = document.getElementById("card-body");
   const profileContainer = document.getElementById("profileID");
+  const divToRemove = document.getElementById("initial");
+  if (divToRemove) {
+    divToRemove.remove();
+  }
   container.innerHTML = "";
   profileContainer.innerHTML = "";
 
@@ -34,6 +47,16 @@ async function performSearch(event) {
   event.target.form.reset();
 }
 
+// "site_admin": false,
+// "name": null,
+// "company": null,
+// "blog": "",
+// "location": null,
+// "email": null,
+// "hireable": null,
+// "bio": null,
+// "twitter_username": null,
+
 function printProfile(profileData, profileContainer) {
   const image = document.createElement("img");
   image.classList.add("user-img");
@@ -42,25 +65,53 @@ function printProfile(profileData, profileContainer) {
   profileContainer.appendChild(image);
   const name = document.createElement("div");
   name.classList.add("profilename");
-  name.textContent =
-    profileData?.name?.charAt(0)?.toUpperCase() + profileData?.name?.slice(1);
+  const name2 = profileData?.name === null ? "" : profileData?.name;
+  name.textContent = name2.charAt(0).toUpperCase() + name2.slice(1) ?? "";
   profileContainer.appendChild(name);
+  const company1 = document.createElement("div");
+  company1.classList.add("profileinfo");
+  company1.textContent =
+    profileData?.company === null ? "HIII" : "Company: " + profileData?.company;
+  const location1 = document.createElement("div");
+  location1.classList.add("profileinfo");
+  location1.textContent =
+    profileData?.location === null ? "" : "Location: " + profileData?.location;
+  const emailadderess = document.createElement("div");
+  emailadderess.classList.add("profileinfo");
+  emailadderess.textContent =
+    profileData?.email === null ? "" : "Email: " + profileData?.email;
+
+  const twitter = document.createElement("div");
+  twitter.classList.add("profileinfo");
+  twitter.textContent =
+    profileData?.twitter_username === null
+      ? ""
+      : "Twitter: " + profileData?.twitter_username;
+
+  profileContainer.appendChild(location1);
+  profileContainer.appendChild(company1);
+  profileContainer.appendChild(twitter);
 }
 
 async function printRepo(data, container, page) {
   const start = (page - 1) * itemsPerPage;
   const end = start + itemsPerPage;
   container.innerHTML = "";
-  for (let i = start; i < end; i++) {
+  for (let i = start; i < end && i < data.length; i++) {
     const card = document.createElement("div");
     card.classList.add("card");
     const lang = await fetchLanguage(data[i]?.languages_url);
+    const link = data[i]?.html_url;
     // console.log(lang, "data printed");
     // console.log(data, "data printed");
 
     const header = document.createElement("div");
     header.classList.add("card-header");
-    header.textContent = data[i]?.name ?? "Name Not Available";
+    const anchor = document.createElement("a");
+    anchor.href = link;
+    anchor.classList.add("anchor");
+    anchor.textContent = data[i]?.name;
+    header.appendChild(anchor);
 
     const description = document.createElement("div");
     description.classList.add("description");
